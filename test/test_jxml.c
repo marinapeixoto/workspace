8e2c668505f74b6676c4fbd5b51bb86e7224bef7
tkorays <tkorays@hotmail.com>
2016-04-17 11:01:26 +0800

    modify test

--- test/test_jxml.c
+++ test/test_jxml.c
@@ -18,8 +18,6 @@ void XML_StrPrint(XML_Str_t* str) {
     printf("\n");
 }
 
-extern XML_RET XML_GetName(const char* addr, int size, XML_Str_t* str);
-extern XML_RET XML_GetStr(const char* addr, int size, XML_Str_t* str);
 extern XML_RET XML_GetAttrPair(const char* addr, int size, int* offset, XML_Str_t* name, XML_Str_t* value);
 
 int main(){
@@ -28,13 +26,6 @@ int main(){
     XML_Str_t value;
     XML_RET ret;
     int offset = 0;
-    ret = XML_GetName("abc= 3eff",9,&str);
-    printf("XML_GetName Ret:%s\n",XML_GetCodeInfo(ret));
-    XML_StrPrint(&str);
-
-    ret = XML_GetStr("\"abcfuck\\\"heheh\"345",19,&str);
-    printf("XML_GetStr Ret:%s\n",XML_GetCodeInfo(ret));
-    XML_StrPrint(&str);
 
     ret = XML_GetAttrPair(" fuck = \"wocao\\\"hehe\"",22,&offset,&name,&value);
     printf("XML_GetAttrPair Ret:%s\n",XML_GetCodeInfo(ret));
